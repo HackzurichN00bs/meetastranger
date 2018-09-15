ff5f0cad2ea70295f42a0f438cfc366830c8d2f4
Dehua Zhu <zhud@phys.ethz.ch>
2018-09-15 12:09:52 +0200

    basic framework

---
+++ MeetStranger/MeetStranger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetStranger
+//
+//  Created by Dehua Zhu on 15.09.18.
+//  Copyright © 2018 HackZurichN00bs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
