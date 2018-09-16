c3a3472fd18335756c83327585ed0b46241cfda0
Dehua Zhu <zhud@phys.ethz.ch>
2018-09-16 08:59:40 +0200

    start button zoom

--- MeetStrangers/MeetStrangers/mapViewViewController.swift
+++ MeetStrangers/MeetStrangers/mapViewViewController.swift
@@ -9,30 +9,49 @@
 import UIKit
 import MapKit
 
-class mapViewViewController: UIViewController {
+class mapViewViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var startmeet: UIButton!
     @IBOutlet weak var stopmeet: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var zoomIn: UIButton!
+    @IBOutlet weak var changeView: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func startmeet_pressed(_ sender: Any) {
         startmeet.isHidden = true
         stopmeet.isHidden = false
+        let userLocation = mapView.userLocation
+        let region = MKCoordinateRegionMakeWithDistance((userLocation.location?.coordinate)!,2000,2000)
+        mapView.setRegion(region,animated: true)
     }
     
     @IBAction func stopbutton_pressed(_ sender: Any) {
         stopmeet.isHidden = true
         startmeet.isHidden = false
+    }
+    
+    @IBAction func zoomIn(_ sender: Any) {
+        let userLocation = mapView.userLocation
+        let region = MKCoordinateRegionMakeWithDistance((userLocation.location?.coordinate)!,2000,2000)
+        mapView.setRegion(region,animated: true)
+    }
+    
+    @IBAction func changeView(_ sender: Any) {
+        if mapView.mapType == MKMapType.standard{
+            mapView.mapType = MKMapType.satellite
+        }else {
+            mapView.mapType = MKMapType.standard
+        }
     }
     
     
